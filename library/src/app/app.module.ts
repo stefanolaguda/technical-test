dca0403d0fb7376abee7a576441644eb9b1dca01
EarthDragon10 <lagudadev@gmail.com>
2023-03-08 18:38:28 +0100

    finishing footer

--- library/src/app/app.module.ts
+++ library/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ArticlesComponent } from './pages/articles/articles.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ArticlesComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
